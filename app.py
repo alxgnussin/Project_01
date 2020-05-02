2c098cf26e66a91df9c6cead54d77683282ee8ce
alxgnussin <alxgns@yandex.ru>
2020-05-02 11:44:07 +0700

    Departure revised

--- app.py
+++ app.py
@@ -10,47 +10,68 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_index():
-    output = render_template('index.html', tours=tours, keys=random_generator())
+    output = render_template(
+        'index.html',
+        tours=tours,
+        keys=random_generator()
+    )
     return output
 
 
 @app.route('/departures/<departure>/')
 def render_departure(departure):
-    output = render_template('departure.html', my_link=departure, tours=tours, price=size("price"),
-                             nights=size("nights"), keys=departure_select(departure))
+    output = render_template(
+        'departure.html',
+        my_link=departure,
+        keys=departure_select(departure)[0],
+        tours=departure_select(departure)[1],
+        price=size(sel_tours=departure_select(departure)[1], select="price"),
+        nights=size(sel_tours=departure_select(departure)[1], select="nights"),
+        tour_from=departure_from(departure)
+    )
     return output
 
 
 @app.route('/tours/<int:tour_id>/')
 def render_tour(tour_id):
-    if tours[tour_id]["departure"] == 'msk':
-        tour_from = 'Из Москвы'
-    elif tours[tour_id]["departure"] == 'spb':
-        tour_from = 'Из Петербурга'
-    elif tours[tour_id]["departure"] == 'nsk':
-        tour_from = 'Из Новосибирска'
-    elif tours[tour_id]["departure"] == 'ekb':
-        tour_from = 'Из Екатеринбурга'
-    elif tours[tour_id]["departure"] == 'kazan':
-        tour_from = 'Из Казани'
-
-    output = render_template('tour.html', key=tour_id, tour_from=tour_from, tours=tours)
+    output = render_template(
+        'tour.html',
+        key=tour_id,
+        tour_from=departure_from(tours[tour_id]["departure"]),
+        tours=tours
+    )
     return output
 
 
-def size(key):
+def departure_from(departure):
+    tour_from = 'из Москвы'
+    if departure == 'spb':
+        tour_from = 'из Петербурга'
+    elif departure == 'nsk':
+        tour_from = 'из Новосибирска'
+    elif departure == 'ekb':
+        tour_from = 'из Екатеринбурга'
+    elif departure == 'kazan':
+        tour_from = 'из Казани'
+    return tour_from
+
+
+def size(sel_tours, select):
     values = []
-    for data in tours.values():
-        values.append(data[key])
+    for data in sel_tours.values():
+        values.append(data[select])
     return values
 
 
 def departure_select(departure):
     keys = []
+    tours_select = {}
     for key, value in tours.items():
         if value['departure'] == departure:
             keys.append(key)
-    return random_generator(tour_id=keys)
+            my_tour = {key: value}
+            tours_select.update(my_tour)
+    return keys, tours_select
 
 
 def random_generator(tour_id=None):
